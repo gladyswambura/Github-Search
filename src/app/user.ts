6e3e6108bb41e0cc5be2b8d7572de31c72579920
gladyswambura <gladyswahito7@gmail.com>
2022-04-16 12:25:07 +0300

    declares the properties of a github user in the user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,25 @@
 export class User {
+    avater_url:string;
+    name:string;
+    username:string;
+    userbio:string;
+    following:number;
+    followers:number;
+    location:string
+
+    // declare the properties of a github user-form
+    constructor(avatar_url: string, name:string, username:string, userbio:string, followers:number, 
+        following:number, location:string, numberofRepositories:number) {
+
+    this.avater_url = avatar_url;
+    this.name = name;
+    this.username = username;
+    this.userbio = userbio;
+    this.followers = followers;
+    this.following = following;
+    this.location = location;
+
+    }
+
+   
 }
