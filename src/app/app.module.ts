49532e8b409e5c57cc4395b4423bb3e0eb8689b7
gladyswambura <gladyswahito7@gmail.com>
2022-04-17 13:22:30 +0300

    importsthe service GitdataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { GitdataService} from './services/gitdata.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [GitdataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
