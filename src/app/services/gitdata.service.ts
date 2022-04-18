664691c67a1f63c854b6b96e0f165348b8c631ef
gladyswambura <gladyswahito7@gmail.com>
2022-04-18 23:35:34 +0300

    Bump: increases the version of the functions

--- src/app/services/gitdata.service.ts
+++ src/app/services/gitdata.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { environment} from 'src/environments/environment';
+import { HttpClient} from '@angular/common/http';
+
+import { User } from '../user';
+import { Repository } from '../repository';
+import { map } from 'rxjs/operators';
+// import { request } from 'https';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitdataService {
+ 
+  myusername!:string
+  constructor(public http: HttpClient) {
+    this.myusername = "gladyswambura"
+  }
+  
+    // interface that displays data after username input
+    findUser(){
+      return this.http.get(`https://api.github.com/users/`+ this.myusername).pipe(map(response => response))
+    }
 
-  constructor() { }
-}
+    findRepository(){
+       return this.http.get(`https://api.github.com/users/`+ this.myusername + '/repos').pipe(map(response => response))
+    }
+
+    newuser(myusername:string) {
+        this.myusername = myusername
+    }
+    // }
+
+    // let headers= new HttpHeaders({'Authorization': 'token' + environment.githubtoken});
+    // let request = environment.baseUrl + whatToSearch;
+  //   let promise = new Promise((resolve, reject) => {
+
+  // }
+
+
+    // interface that displays data after repository input
+  //   showRepositories(user:any){
+  //   interface GithubApi{
+  //     html_url:string,
+  //     description:string,
+  //     language:string,
+  //     homepage:string
+  //   }
+  //  }
+
+
+     }
